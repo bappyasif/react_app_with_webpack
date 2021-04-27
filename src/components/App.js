bb921c18f9f96bb6ebaa2e737f3e384e98cf69f1
bappyasif <bappyasif@hotmail.com>
2021-04-28 01:54:43 +0600

    Initial and final commit to this demo of React App setup with Webpack.

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+export default class App extends Component {
+    render() {
+        return(
+            <div>
+                <h4>React App Boilerplate Setup With Webpack</h4>
+            </div>
+        )
+    }
+}
